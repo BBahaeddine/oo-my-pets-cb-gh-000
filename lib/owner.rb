fb1b9a943b3437ad7e4b2760fecd97375b8523aa
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-13 10:48:47 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,54 @@
+require "pry"
 class Owner
-  # code goes here
+  attr_accessor :name
+  attr_reader :species
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    # @species = "human"
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@all.length
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+  def species(specie)
+    @species = "human"
+    # say_species
+  end
+  def say_species
+    @species
+  end
+  
+  def pets 
+    pets = {
+      :fishes => [],
+      :dogs => [],
+      :cats => []
+    }
+    return pets
+  end
+  
+  def buy_fish(name)
+    fish = Fish.new(name)
+    self.pets[:fishes] << fish
+    # binding.pry
+  end
+  
+  def buy_dog
+  end 
+  
+  def buy_cat
+  end
 end
+
+owner = Owner.new("haha")
